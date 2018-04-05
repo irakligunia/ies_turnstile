0e56e54983d55e3b5a5aab1f29d8b42042054b54
Irakli <irakli.gunia@gmail.com>
2018-04-05 18:43:15 +0400

    განახლდა ცვლადის სახელები

--- ies_get_turnstile_data.py
+++ ies_get_turnstile_data.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
+import MySQLdb
 import io
 from ctypes import *
-
-ip = "10.0.0.245"
-
-params = "protocol=TCP,ipaddress=" + ip + ",port=4370,timeout=4000,passwd="
+turnstile_ip = "10.0.0.245"
+mysql_ip = "10.0.0.237"
+mysql_user = "turnstile"
+mysql_pass = "turnstile"
+mysql_db = "ies_inventari"
+turnstile_conn_params = "protocol=TCP,ipaddress=" + turnstile_ip + ",port=4370,timeout=4000,passwd="
 
 commpro = windll.LoadLibrary("plcommpro.dll")
 
@@ -26,41 +29,36 @@ def turnstile_time_to_mysql_time(time):
 
 
 
-constr = create_string_buffer(params) 
-hcommpro = commpro.Connect(constr)
+constr = create_string_buffer(turnstile_con_params) 
+conn_hendler = commpro.Connect(constr)
 
-if hcommpro != 0:
+if conn_hendler != 0:
 	print "Connected successfully to turnstile..."
 else:
-	print "Cannot connect to turnstile via IP(" + ip +") address"
-	commpro.Disconnect(hcommpro)
+	print "Cannot connect to turnstile via IP(" + turnstile_ip +") address"
+	commpro.Disconnect(conn_hendler)
 	exit()
 
 
-table = "transaction"		# Download the user data from the user table
+table = "transaction" # Download the user data from the user table
 fieldname = "*"		# Download all field information in the table
 query_filter = ""	# Have no filtering conditions and thus download all information
 options = ""	
 query_buf = create_string_buffer(4*1024*1024)
 query_table = create_string_buffer(table)
-
-
-# print type(query_table), query_table
-# exit()
-
 query_fieldname = create_string_buffer(fieldname)
 query_filter = create_string_buffer(query_filter)
 query_options = create_string_buffer(options)
-ret = commpro.GetDeviceData(hcommpro, query_buf, 4*1024*1024, query_table, query_fieldname, query_filter, query_options)
-if ret>0:
-	print query_buf.value
+ret = commpro.GetDeviceData(conn_hendler, query_buf, 4*1024*1024, query_table, query_fieldname, query_filter, query_options)
+
+if ret > 0:
 	print str(ret) + ' ' + table + ' records found!'
-	# print query_buf.value
 else:
-	print "Error - 1"
+	print "Error (GetDeviceData)- " + str(ret)
+	commpro.Disconnect(conn_hendler)
 	exit()
-commpro.Disconnect(hcommpro)
-hcommpro = 0
+commpro.Disconnect(conn_hendler)
+conn_hendler = 0
 
 lines = query_buf.value.split('\r\n')
 del lines[0]
