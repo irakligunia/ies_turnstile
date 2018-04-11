3244de9eaa23d0a8b6f0afd9f7ba9da37af734dc
Irakli <irakli.gunia@gmail.com>
2018-04-11 16:14:18 +0400

    დაემატა mysql-თან დაკავშირება

--- ies_get_turnstile_data.py
+++ ies_get_turnstile_data.py
@@ -1,15 +1,32 @@
 # -*- coding: utf-8 -*-
+import os
 import MySQLdb
 import io
 from ctypes import *
+
 turnstile_ip = "10.0.0.245"
-mysql_ip = "10.0.0.237"
+mysql_ip = "10.0.0.2379"
 mysql_user = "turnstile"
 mysql_pass = "turnstile"
 mysql_db = "ies_inventari"
 turnstile_conn_params = "protocol=TCP,ipaddress=" + turnstile_ip + ",port=4370,timeout=4000,passwd="
 
-commpro = windll.LoadLibrary("plcommpro.dll")
+# სკრიპტის მისამართი
+script_path = os.path.dirname(os.path.realpath(__file__))
+# შევცვალოთ სამუშაო დირექტორია იმ მისამართზე სადაც გვაქვს ies_get_turnstile_data.py
+os.chdir(script_path)
+commpro = windll.LoadLibrary(script_path + "/plcommpro.dll")
+
+try:
+	# Mysql - თან დაკავშირება
+	db = MySQLdb.connect(mysql_ip, mysql_user, mysql_pass, mysql_db)
+	cursor = db.cursor()
+except:
+	print("Cannot connect to Mysql on %s" % mysql_ip)
+	exit()
+
+print("Connected successfully to Mysql on %s" % mysql_ip)
+
 
 def turnstile_time_to_mysql_time(time):
 	time = int(time)
@@ -29,7 +46,7 @@ def turnstile_time_to_mysql_time(time):
 
 
 
-constr = create_string_buffer(turnstile_con_params) 
+constr = create_string_buffer(turnstile_conn_params) 
 conn_hendler = commpro.Connect(constr)
 
 if conn_hendler != 0:
@@ -37,12 +54,13 @@ if conn_hendler != 0:
 else:
 	print "Cannot connect to turnstile via IP(" + turnstile_ip +") address"
 	commpro.Disconnect(conn_hendler)
+	db.close()
 	exit()
 
 
 table = "transaction" # Download the user data from the user table
 fieldname = "*"		# Download all field information in the table
-query_filter = ""	# Have no filtering conditions and thus download all information
+query_filter = "Cardno=3944526"	# Have no filtering conditions and thus download all information
 options = ""	
 query_buf = create_string_buffer(4*1024*1024)
 query_table = create_string_buffer(table)
@@ -56,6 +74,7 @@ if ret > 0:
 else:
 	print "Error (GetDeviceData)- " + str(ret)
 	commpro.Disconnect(conn_hendler)
+	db.close()
 	exit()
 commpro.Disconnect(conn_hendler)
 conn_hendler = 0
@@ -64,6 +83,7 @@ lines = query_buf.value.split('\r\n')
 del lines[0]
 del lines[-1]
 for line in lines:
+	# print line
 	words = line.split(',')
 
 	# print words
